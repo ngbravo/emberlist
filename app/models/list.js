83cde74e5fa8cd6b6726b65fe8f8b60b16971c50
Nikolas Bravo <nikolasbra@gmail.com>
2015-10-17 15:03:55 -0300

    uses local storage

--- app/models/list.js
+++ app/models/list.js
@@ -4,19 +4,4 @@ var List = DS.Model.extend({
   name: DS.attr('string')
 });
 
-List.reopenClass({
-
-FIXTURES:[
-  {
-    id: 1,
-    name: 'una lista'
-  },
-  {
-    id: 2,
-    name: 'otra lista'
-  }
-]
-
-});
-
 export default List;
